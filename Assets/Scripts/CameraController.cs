afa44c1358db144df59619fec360d56e65062386
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-08-22 19:35:32 +0200

    Add camera size controll

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // change size to zoom in/out
+    // clamp values to min and max
+    // mouse wheel scrolling
+    private void LateUpdate()
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * Camera.main.orthographicSize, 2.5f, 20f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
